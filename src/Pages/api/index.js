9b0aa5dbdbc20cf0007f3e0417009570f25575e4
K. J. B. Thilakarathna <168623163+KasunJanith@users.noreply.github.com>
2026-09-09 10:22:27 +0530

    changed the base url

--- src/Pages/api/index.js
+++ src/Pages/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "http://127.0.0.1:8000/api/v1" });
+const api = axios.create({ baseURL: "https://backoffice.884.lk/pyapi/api/v1" });
 
 export const uploadArchive = (file, date) => {
   const formData = new FormData();
